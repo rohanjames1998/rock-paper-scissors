bd041e394991386f9ecd273e17ce9f27d0da7822
Rohan James <rohanjames9282@gmail.com>
2022-07-17 14:12:30 +0530

    Fix promt not showing each round

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function getComputerChoice() {
    const rng = Math.floor(Math.random() * 3) + 1;
    
-   if (rng === 1){
+   if (rng == 1){
     return "Rock";
    }
-   if (rng === 2){
+   if (rng == 2){
     return "Paper";
    }
    
@@ -17,79 +17,98 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
 switch (true){
-    case playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock":
-    return "It's a tie!!";
+    case playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock":
+    return "It's a Tie!!";
         break;
 
-    case playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper":
-        return "It's a tie!!";
+    case playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper":
+        return "It's a Tie!!";
         break;
 
-    case playerSelection.toLowerCase() === "scissor" && computerSelection.toLowerCase() === "scissor":
+    case playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "scissor":
             return "It's a Tie!!";
         break;
 
-    case playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "paper":
+    case playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase()== "paper":
             return "You Lose! Paper beats Rock.";
             break;
     
-    case playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "scissor":
+    case playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase()== "scissor":
             return "You Win! Rock beats Scissor.";
             break;
       
-    case playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "rock":
+    case playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase()== "rock":
                 return "You Win! Paper beats Rock.";
                 break;
 
-    case playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "scissor":
+    case playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase()== "scissor":
             return "You Lose! Scissor beats Paper.";
             break;
     
-    case playerSelection.toLowerCase() === "scissor" && computerSelection.toLowerCase()=== "paper":
+    case playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase()== "paper":
                 return "You Win! Scissor beats Paper.";
                 break;
 
-    case playerSelection.toLowerCase() === "scissor" && computerSelection.toLowerCase()=== "rock":
+    case playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase()== "rock":
                  return "You Lose! Rock beats Scissor.";
                  break;
         
 }
 }
 
-const playerSelection = prompt("Rock, Paper, or Scissor?")
 
-const computerSelection = getComputerChoice();
 
 
 // This function runs the game for 5 rounds and returns the winner in the end.
 
 function game(){
 
-    const tieCounter = 0;
-    const winCounter = 0;
-    const lossCounter =  0;
+    let playerSelection = prompt("Rock, Paper, or Scissor?");
+
+    let computerSelection = getComputerChoice();
+
+    let tieCounter = 0;
+    let winCounter = 0;
+    let lossCounter = 0;
+
 
     for (let i = 0; i < 5; i++){
         //Message for each round.
-        playRound();
-        console.log(playRound(playerSelection, computerSelection));
-
-        console.log(`Scores:`);
-
-        console.log(`Player Score = ${winCounter}`);
-        console.log(`Computer Score = ${lossCounter}`);
-        console.log(`Ties = ${tieCounter}`);
-
+        playerSelection = prompt("Rock, Paper, or Scissor?");
+        computerSelection = getComputerChoice();
+       
         //Calculating each rounds wins, losses, or ties.
-        if (playRound.includes("Win")){
+        if (playRound(playerSelection, computerSelection).includes("Win")){
             winCounter++;
         }
 
-        if (playRound.includes("Lose")){
+        if (playRound(playerSelection, computerSelection).includes("Lose")){
             lossCounter++;
         }
-        tieCounter++;
+        
+        if (playRound(playerSelection, computerSelection).includes("Tie")){
+            tieCounter++;
+        }
+
+        // Each rounds display message
+
+        console.log(`Scores:`);
+        console.log(`Player Score = ${winCounter}`);
+        console.log(`Computer Score = ${lossCounter}`);
+        console.log(`Ties = ${tieCounter}`);
+        console.log(playRound());
     }
+
+    //For if it's a tie.
+    if(tieCounter == 4 || 
+        tieCounter == 3 && winCounter == 1 && lossCounter == 1||
+        tieCounter == 2 && winCounter == 2 && lossCounter == 2){
+        console.log(`Player Score = ${winCounter}`);
+        console.log(`Computer Score = ${lossCounter}`);
+        console.log(`Ties = ${tieCounter}`);
+        console.log("And That's a Tie... >_<");
+        }
+
     // For if player wins
     if (winCounter > lossCounter){
         console.log(`Player Score = ${winCounter}`);
@@ -105,9 +124,7 @@ function game(){
         console.log("Better Luck Next Time :(");
     }
 
-    //For if it's a tie.
-    console.log(`Player Score = ${winCounter}`);
-    console.log(`Computer Score = ${lossCounter}`);
-    console.log(`Ties = ${tieCounter}`);
-    console.log("And That's a Tie... >_<")
+    
 }
+
+game();
