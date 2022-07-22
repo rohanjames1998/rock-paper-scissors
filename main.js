42f93d51117c33cbe65327d5a3941a0c5d422082
Rohan James <rohanjames9282@gmail.com>
2022-07-22 15:16:14 +0530

    Fix score not showing in scoreboard

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function getComputerChoice() {
 const buttons = document.querySelectorAll(".game-button");
 buttons.forEach((item) =>
   addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
     const selection = e.target.value;
     game(selection);
   })
@@ -36,55 +37,71 @@ function game(selection) {
       break;
 
     case playerSelection == "rock" && computerSelection == "paper":
-      displayMsg.textContent = "You Lose! Paper beats Rock.";
-      
+       displayMsg.textContent = "You Lose! Paper beats Rock.";
+  round();
+
+
       break;
 
     case playerSelection == "rock" && computerSelection == "scissor":
-      displayMsg.textContent = "You Win! Rock beats Scissor.";
+       displayMsg.textContent = "You Win! Rock beats Scissor.";
+  round();
+
+       
 
       break;
 
     case playerSelection == "paper" && computerSelection == "rock":
-      displayMsg.textContent = "You Win! Paper beats Rock.";
+       displayMsg.textContent = "You Win! Paper beats Rock.";
 
       break;
 
     case playerSelection == "paper" && computerSelection == "scissor":
-      displayMsg.textContent = "You Lose! Scissor beats Paper.";
+       displayMsg.textContent = "You Lose! Scissor beats Paper.";
 
       break;
 
     case playerSelection == "scissor" && computerSelection == "paper":
-      displayMsg.textContent = "You Win! Scissor beats Paper.";
+       displayMsg.textContent = "You Win! Scissor beats Paper.";       
 
       break;
 
     case playerSelection == "scissor" && computerSelection == "rock":
-      displayMsg.textContent = "You Lose! Rock beats Scissor.";
+       displayMsg.textContent = "You Lose! Rock beats Scissor.";       
 
       break;
-  }
-}
 
+      
+  }
+
+}
 // This function does 3 things:
-//Display all scores and messages;
-// Count rounds choose a winner;
+//Display all scores;
+// Count until someone reaches 5 points;
 // Call end function after finishing;
 
-function round(displayMsg, playerScore, computerScore) {
-  let updatedMsg = document.querySelector(".displayMsg");
-  let newPlayerScore = document.querySelector(".playerScore");
-  let newComputerScore = document.querySelector(".computerScore");
+function round() {
 
-  updatedMsg.textContent = displayMsg;
-  newPlayerScore.textContent = playerScore;
-  newComputerScore.textContent = computerScore;
+  let playerScore = document.querySelector(".playerScore");
+  let computerScore = document.querySelector(".computerScore");
+  let displayMsg = document.querySelector(".displayMsg");
 
-  if (newPlayerScore == 5 || newComputerScore == 5) {
-    end(displayMsg, playerScore, computerScore);
+
+  
+  if (displayMsg.textContent.includes("You Win")){
+    playerScore.textContent++; 
   }
+
+  if (displayMsg.textContent.includes("Lose")){
+    computerScore.textContent++;
+  }
+  
+
+  
 }
+
+
+
 
 function end() {
   console.log("End");
