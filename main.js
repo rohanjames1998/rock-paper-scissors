8732dbb7d3045f09c9354356342cd2ccd73cb690
Rohan James <rohanjames9282@gmail.com>
2022-07-22 12:17:50 +0530

    Finish game function

--- main.js
+++ main.js
@@ -17,49 +17,74 @@ function getComputerChoice() {
   const buttons = document.querySelectorAll(".game-button");
   buttons.forEach(item => addEventListener("click", e => {
     const selection = e.target.value;
-    round(selection);
+    game(selection);
 }));
 
 
-function round(selection){
+function game(selection){
 
     let playerSelection = selection;
     let computerSelection = getComputerChoice();
+    let displayMsg = "Score 5 Points to Win!!";
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // This switch function compares computer selection with player selection, increase the scores, and update display message for each round through round() function.
     switch(true){
 
     case playerSelection == computerSelection:
-            return "It's a Tie >_<";
+            displayMsg = "It's a Tie >_<";
+         round(displayMsg, computerScore, playerScore);
+          break;
 
      case playerSelection == "rock" && computerSelection == "paper":
 
-      return "You Lose! Paper beats Rock.";
+      displayMsg = "You Lose! Paper beats Rock.";
+      computerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "rock" && computerSelection == "scissor":
 
-      return "You Win! Rock beats Scissor.";
+      displayMsg = "You Win! Rock beats Scissor.";
+      playerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "paper" && computerSelection == "rock":
 
-      return "You Win! Paper beats Rock.";
+      displayMsg = "You Win! Paper beats Rock.";
+      playerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "paper" && computerSelection == "scissor":
 
-      return "You Lose! Scissor beats Paper.";
+      displayMsg = "You Lose! Scissor beats Paper.";
+      computerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "scissor" && computerSelection == "paper":
 
-      return "You Win! Scissor beats Paper.";
+      displayMsg = "You Win! Scissor beats Paper.";
+      playerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "scissor" && computerSelection == "rock":
 
-      return "You Lose! Rock beats Scissor.";
+      displayMsg = "You Lose! Rock beats Scissor.";
+      computerScore++;
+      round(displayMsg, computerScore, playerScore);
       break;
     }
-
+    end(displayMsg, playerScore, computerScore);
 }
+
+
+// This function does 3 things:
+  //Display all scores and messages;
+  // Count rounds and choose a winner
+
 
