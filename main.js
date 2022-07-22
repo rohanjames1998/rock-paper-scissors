0bd2b121deb992c4b621a85318f38f81a8f5e490
Rohan James <rohanjames9282@gmail.com>
2022-07-22 13:21:34 +0530

    Seperated round function from game function

--- main.js
+++ main.js
@@ -1,96 +1,91 @@
-
-
+// Getting computer choice.
 
 function getComputerChoice() {
-    const rng = Math.floor(Math.random() * 3) + 1;
-  
-    if (rng == 1) {
-      return "rock";
-    }
-    if (rng == 2) {
-      return "paper";
-    }
-  
-    return "scissor";
+  const rng = Math.floor(Math.random() * 3) + 1;
+
+  if (rng == 1) {
+    return "rock";
+  }
+  if (rng == 2) {
+    return "paper";
   }
 
-  const buttons = document.querySelectorAll(".game-button");
-  buttons.forEach(item => addEventListener("click", e => {
+  return "scissor";
+}
+
+// Adding event listener to each button
+const buttons = document.querySelectorAll(".game-button");
+buttons.forEach((item) =>
+  addEventListener("click", (e) => {
     const selection = e.target.value;
     game(selection);
-}));
+  })
+);
 
+// Main function that plays the game.
+function game(selection) {
+  let playerSelection = selection;
+  let computerSelection = getComputerChoice();
+  let displayMsg = document.querySelector(".displayMsg");
 
-function game(selection){
-
-    let playerSelection = selection;
-    let computerSelection = getComputerChoice();
-    let displayMsg = "Score 5 Points to Win!!";
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // This switch function compares computer selection with player selection, increase the scores, and update display message for each round through round() function.
-    switch(true){
-
+  // This switch function compares computer selection with player selection, increase the scores, and update display message for each round through round() function.
+  switch (true) {
     case playerSelection == computerSelection:
-            displayMsg = "It's a Tie >_<";
-         round(displayMsg, computerScore, playerScore);
-          break;
+      displayMsg.textContent = "It's a Tie >_<";
 
-     case playerSelection == "rock" && computerSelection == "paper":
+      break;
 
-      displayMsg = "You Lose! Paper beats Rock.";
-      computerScore++;
-      round(displayMsg, computerScore, playerScore);
+    case playerSelection == "rock" && computerSelection == "paper":
+      displayMsg.textContent = "You Lose! Paper beats Rock.";
+      
       break;
 
     case playerSelection == "rock" && computerSelection == "scissor":
+      displayMsg.textContent = "You Win! Rock beats Scissor.";
 
-      displayMsg = "You Win! Rock beats Scissor.";
-      playerScore++;
-      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "paper" && computerSelection == "rock":
+      displayMsg.textContent = "You Win! Paper beats Rock.";
 
-      displayMsg = "You Win! Paper beats Rock.";
-      playerScore++;
-      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "paper" && computerSelection == "scissor":
+      displayMsg.textContent = "You Lose! Scissor beats Paper.";
 
-      displayMsg = "You Lose! Scissor beats Paper.";
-      computerScore++;
-      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "scissor" && computerSelection == "paper":
+      displayMsg.textContent = "You Win! Scissor beats Paper.";
 
-      displayMsg = "You Win! Scissor beats Paper.";
-      playerScore++;
-      round(displayMsg, computerScore, playerScore);
       break;
 
     case playerSelection == "scissor" && computerSelection == "rock":
+      displayMsg.textContent = "You Lose! Rock beats Scissor.";
 
-      displayMsg = "You Lose! Rock beats Scissor.";
-      computerScore++;
-      round(displayMsg, computerScore, playerScore);
       break;
-    }
-    end(displayMsg, playerScore, computerScore);
+  }
 }
-
 
 // This function does 3 things:
-  //Display all scores and messages;
-  // Count rounds choose a winner;
-  // Call end function after finishing;
+//Display all scores and messages;
+// Count rounds choose a winner;
+// Call end function after finishing;
 
+function round(displayMsg, playerScore, computerScore) {
+  let updatedMsg = document.querySelector(".displayMsg");
+  let newPlayerScore = document.querySelector(".playerScore");
+  let newComputerScore = document.querySelector(".computerScore");
 
-function round(displayMsg, playerScore, computerScore){
+  updatedMsg.textContent = displayMsg;
+  newPlayerScore.textContent = playerScore;
+  newComputerScore.textContent = computerScore;
 
-  
+  if (newPlayerScore == 5 || newComputerScore == 5) {
+    end(displayMsg, playerScore, computerScore);
+  }
 }
 
+function end() {
+  console.log("End");
+}
